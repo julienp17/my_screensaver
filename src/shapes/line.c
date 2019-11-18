445d684fa018fe9ae0f6aa07db6274813fe4b526
Julien Pause <julien.pause@epitech.eu>
2019-11-18 14:19:07 +0400

    Add line shape

---
+++ src/shapes/line.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2019
+** My Screensaver
+** File description:
+** Every function needed to draw a line
+*/
+
+#include <stdlib.h>
+#include <SFML/Graphics.h>
+#include "framebuffer.h"
+#include "shapes.h"
+
+void draw_line(framebuffer_t *framebuffer, sfVector2f point_a,
+                sfVector2f point_b, sfColor color)
+{
+    int dx = point_b.x - point_a.x;
+    int dy = point_b.y - point_a.y;
+    float x = point_a.x;
+    float y = point_a.y;
+    unsigned int steps = (abs(dx) > abs(dy)) ? abs(dx) : abs(dy);
+
+    for (unsigned int i = 0 ; i < steps + 1 ; i = i + 1) {
+        my_put_pixel(framebuffer, x, y, color);
+        x = x + (dx / (float)steps);
+        y = y + (dy / (float)steps);
+    }
+}
