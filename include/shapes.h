b7c5238ead1d4374ab221d41dae4fcf670d1c985
Julien Pause <julien.pause@epitech.eu>
2019-11-18 14:20:07 +0400

    Add shapes.h

---
+++ include/shapes.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2019
+** My Screensaver
+** File description:
+** Header file for shapes
+*/
+
+#ifndef SHAPES_H_
+    #define SHAPES_H_
+
+    #include <SFML/Graphics.h>
+    #include "framebuffer.h"
+
+    void draw_line(framebuffer_t *framebuffer, sfVector2f point_a,
+                sfVector2f point_b, sfColor color);
+#endif
