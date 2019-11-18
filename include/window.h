f2de078fee3f8a6169987cc718f353972a592765
Julien Pause <julien.pause@epitech.eu>
2019-11-18 10:33:03 +0400

    Add window.h

---
+++ include/window.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2019
+** My Screensaver
+** File description:
+** Header file for windows
+*/
+
+#ifndef WINDOW_H_
+    #define WINDOW_H_
+
+    #define W_WIDTH         800
+    #define W_HEIGHT        600
+    #define W_BPP           32
+    #define W_TITLE         "My ScreenSaver"
+#endif
