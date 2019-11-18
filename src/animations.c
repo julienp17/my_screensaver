cfa101ee066defecaf143fa697322c5a7df53d8e
Julien Pause <julien.pause@epitech.eu>
2019-11-18 10:33:37 +0400

    Add animations.c

---
+++ src/animations.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2019
+** My Screensaver
+** File description:
+** Displays animations
+*/
+
+#include <stddef.h>
+#include "animations.h"
+#include "framebuffer.h"
+
+void (*get_animation_by_id(unsigned int nb))(framebuffer_t*)
+{
+    unsigned int i = 1;
+    void (*animation)(framebuffer_t*) = NULL;
+    void (*animations[])(framebuffer_t*) = {
+        &purple_rain,
+        &starfield
+    };
+
+    while (i < nb)
+        i = i + 1;
+    animation = animations[i];
+    return (animation);
+}
