8f34939c6056e5bcf8e4857723db2eb876aff826
Julien Pause <julien.pause@epitech.eu>
2019-11-18 10:35:26 +0400

    Add starfield animation

---
+++ src/animations/starfield.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2019
+** My Screensaver
+** File description:
+** Displays a simulation of a starfield
+*/
+
+#include "framebuffer.h"
+
+void starfield(framebuffer_t *framebuffer)
+{
+    (void)framebuffer;
+    return;
+}
