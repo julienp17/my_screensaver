8d0292d33a063cab80f043a1cc1513df09f97c10
Julien Pause <julien.pause@epitech.eu>
2019-11-18 14:23:05 +0400

    Display a line in purple_rain as a display test

--- src/animations/purple_rain.c
+++ src/animations/purple_rain.c
@@ -5,12 +5,18 @@
 ** Displays a simulation of falling purple rain
 */
 
-
 #include "window.h"
 #include "framebuffer.h"
 #include "shapes.h"
 
 void purple_rain(framebuffer_t *framebuffer)
 {
-    
+    sfVector2f point_a;
+    sfVector2f point_b;
+
+    point_a.x = W_WIDTH / 2;
+    point_a.y = 0;
+    point_b.x = W_WIDTH / 2;
+    point_b.y = W_HEIGHT;
+    draw_line(framebuffer, point_a, point_b, sfRed);
 }
