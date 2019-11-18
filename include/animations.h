7cd07a138aa602cca5085d40cf5082dffd80c184
Julien Pause <julien.pause@epitech.eu>
2019-11-18 10:32:42 +0400

    Add animations.h

---
+++ include/animations.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2019
+** My Screensaver
+** File description:
+** Header file for animations
+*/
+
+#ifndef ANIMATIONS_H_
+    #define ANIMATIONS_H_
+
+    #include "framebuffer.h"
+
+    void purple_rain(framebuffer_t *framebuffer);
+    void starfield(framebuffer_t *framebuffer);
+#endif
