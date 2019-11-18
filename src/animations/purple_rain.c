eea142661804733b965a0663350197a00aa52dee
Julien Pause <julien.pause@epitech.eu>
2019-11-18 19:41:42 +0400

    Test purple_rain to display a moving line

--- src/animations/purple_rain.c
+++ src/animations/purple_rain.c
@@ -5,11 +5,12 @@
 ** Displays a simulation of falling purple rain
 */
 
+#include "my_screensaver.h"
 #include "window.h"
 #include "framebuffer.h"
 #include "shapes.h"
 
-void purple_rain(framebuffer_t *framebuffer)
+void purple_rain(window_t *window)
 {
     sfVector2f point_a;
     sfVector2f point_b;
@@ -18,5 +19,11 @@ void purple_rain(framebuffer_t *framebuffer)
     point_a.y = 0;
     point_b.x = W_WIDTH / 2;
     point_b.y = W_HEIGHT;
-    draw_line(framebuffer, point_a, point_b, sfRed);
+    draw_line(window->framebuffer, point_a, point_b, sfRed);
+    while (sfRenderWindow_isOpen(window->window)) {
+        poll_events(window->window);
+        draw_line(window->framebuffer, point_a, point_b, sfRed);
+        point_a.x = point_a.x + 1.0;
+        window_refresh(window);
+    }
 }
