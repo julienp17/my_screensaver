b089b3c469e456b259a4be5cf86bdc31eaacf175
Julien Pause <julien.pause@epitech.eu>
2019-11-18 10:28:27 +0400

    Check for errors in main and call display_screensaver if none are found

--- src/main.c
+++ src/main.c
@@ -5,7 +5,20 @@
 ** Main file for my_screensaver
 */
 
+#include "my_screensaver.h"
+#include "my.h"
+
 int main(int ac, char **av)
 {
-    return (0);
+    int error_code = 0;
+    int animation_id = 0;
+
+    error_code = check_args(ac, av);
+    if (error_code == MY_EXIT_OPTION)
+        return (MY_EXIT_SUCCESS);
+    else if (error_code == MY_EXIT_FAILURE)
+        return (MY_EXIT_FAILURE);
+    animation_id = my_strnum_to_uint(av[1]);
+    error_code = display_screensaver(animation_id);
+    return (MY_EXIT_SUCCESS);
 }
