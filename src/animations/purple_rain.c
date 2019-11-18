874895a2d855efd3956d33e10deb3efd5c2a7e04
Julien Pause <julien.pause@epitech.eu>
2019-11-18 11:26:47 +0400

    Add empty purple_rain animation

---
+++ src/animations/purple_rain.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2019
+** My Screensaver
+** File description:
+** Displays a simulation of falling purple rain
+*/
+
+#include "framebuffer.h"
+
+void purple_rain(framebuffer_t *framebuffer)
+{
+    (void)framebuffer;
+    return;
+}
