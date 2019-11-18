0b9972d0958f1aac5513e21d4874b8a19ccb0d7e
Julien Pause <julien.pause@epitech.eu>
2019-11-18 14:19:53 +0400

    Send the animation function pointer from main rather than create it in display_screensaver

--- src/main.c
+++ src/main.c
@@ -5,13 +5,19 @@
 ** Main file for my_screensaver
 */
 
+#include <stddef.h>
+#include "animations.h"
+#include "framebuffer.h"
 #include "my_screensaver.h"
 #include "my.h"
+
+void (*get_animation_by_id(unsigned int nb))(framebuffer_t*);
 
 int main(int ac, char **av)
 {
     int error_code = 0;
-    int animation_id = 0;
+    unsigned int animation_id = 0;
+    void (*play_animation)(framebuffer_t*) = NULL;
 
     error_code = check_args(ac, av);
     if (error_code == MY_EXIT_OPTION)
@@ -19,6 +25,7 @@ int main(int ac, char **av)
     else if (error_code == MY_EXIT_FAILURE)
         return (MY_EXIT_FAILURE);
     animation_id = my_strnum_to_uint(av[1]);
-    error_code = display_screensaver(animation_id);
+    play_animation = get_animation_by_id(animation_id);
+    display_screensaver(play_animation);
     return (MY_EXIT_SUCCESS);
 }
