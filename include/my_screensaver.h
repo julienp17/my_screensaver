0d4cfd19e700dbca75f2308143f817569c9c43e5
Julien Pause <julien.pause@epitech.eu>
2019-11-18 10:32:54 +0400

    Add my_screensaver.h

---
+++ include/my_screensaver.h
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2019
+** My screensaver
+** File description:
+** Header file for my screensaver
+*/
+
+#ifndef MY_SCREENSAVER_H_
+    #define MY_SCREENSAVER_H_
+
+    #include <stdbool.h>
+
+    #define MY_EXIT_SUCCESS             0
+    #define MY_EXIT_FAILURE             84
+    #define MY_EXIT_OPTION              92
+
+    #define SHORT_PROGRAM_DESCRIPTION   "animation rendering in a CSFML window."
+
+    int check_args(int ac, char **av);
+    void print_help(void);
+    void print_usage(void);
+    void print_options(void);
+    bool check_option(char *str);
+
+    int display_screensaver(unsigned int animation_id);
+#endif
