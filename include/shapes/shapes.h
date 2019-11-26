827cd4bab7cda49be5deaad8ad13ced6da4b9b0a
Julien Pause <julien.pause@epitech.eu>
2019-11-26 14:10:51 +0400

    Change draw circle to accept sfVector2f center instead of sfVector2i

--- include/shapes/shapes.h
+++ include/shapes/shapes.h
@@ -13,6 +13,6 @@
 
     void draw_line(framebuffer_t *framebuffer, sfVector2f point_a,
                 sfVector2f point_b, sfColor color);
-    void draw_circle(framebuffer_t *framebuffer, sfVector2i center,
-                unsigned int radius, sfColor color)
+    void draw_circle(framebuffer_t *framebuffer, sfVector2f center,
+                unsigned int radius, sfColor color);
 #endif
