f12cd41699f581a38c2fb0a9f5145b1afb42071b
Julien Pause <julien.pause@epitech.eu>
2019-11-26 22:29:40 +0400

    Add radar animation (no loop)

---
+++ include/animations/radar.h
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2019
+** My Screensaver
+** File description:
+** Header file for radar
+*/
+
+#ifndef RADAR_H_
+    #define RADAR_H_
+
+    #include <stdbool.h>
+    #include "window.h"
+    #include "line.h"
+
+    #define LINES_OPACITY       255
+    #define DEEP_SKY_BLUE4      0, 95, 175
+    #define DEEP_PINK4          95, 0, 95
+    #define DARK_ORANGE         255, 135, 0
+    #define DARK_GREEN          0, 150, 0
+
+    bool update_lines_step_1(window_t *window, line_t *line1, line_t *line2);
+#endif
--- src/animations/radar.c
+++ src/animations/radar.c
@@ -5,40 +5,75 @@
 ** Radar
 */
 
+#include <stdbool.h>
 #include <stdlib.h>
 #include <SFML/Graphics.h>
+#include "radar.h"
 #include "line.h"
 #include "window.h"
 
+#include <stdio.h>
+
 void radar(window_t *window)
 {
-    sfColor blue = {0, 95, 175, 255};
-    sfColor magenta = {255, 95, 215, 255};
-    line_t *line1 = line_create((sfVector2f){window->width / 2, 0},
-                                (sfVector2f){window->width, 0}, magenta);
+    line_t *line1 = line_create((sfVector2f) {window->width / 2, 0},
+                                (sfVector2f) {window->width, 0},
+                                (sfColor) {DEEP_PINK4, LINES_OPACITY});
     line_t *line2 = line_create((sfVector2f){window->width / 2, window->height},
-                                (sfVector2f){0, window->height}, blue);
+                                (sfVector2f){0, window->height},
+                                (sfColor) {DEEP_SKY_BLUE4, LINES_OPACITY});
 
     while (sfRenderWindow_isOpen(window->window)) {
         poll_events(window->window);
-        draw_line(window->framebuffer, line1->point_a, line1->point_b, line1->color);
-        draw_line(window->framebuffer, line2->point_a, line2->point_b, line2->color);
-        if (line1->point_b.y < window->height) {
-            line1->point_b.y++;
-        } else {
-            line1->point_b.x = 0;
-            line1->point_b.y = 0;
-            line1->color = (sfColor){255, 135, 0, 255};
-        }
-        if (line2->point_b.y > 0) {
-            line2->point_b.y--;
-        } else {
-            line2->point_b.x = window->width;
-            line2->point_b.y = window->height;
-            line2->color = (sfColor){215, 255, 135, 255};
+        draw_line(window->framebuffer, line1->point_a, line1->point_b,
+                line1->color);
+        draw_line(window->framebuffer, line2->point_a, line2->point_b,
+                line2->color);
+        if (!update_lines_step_1(window, line1, line2)) {
+            if (line1->point_b.y < window->height / 2) {
+                line1->point_b.x += 0.5;
+                line1->point_b.y += 0.75;
+            }
+            if (line2->point_b.y > window->height / 2) {
+                line2->point_b.x -= 0.5;
+                line2->point_b.y -= 0.75;
+            }
         }
         window_refresh(window, NULL);
     }
     free(line1);
     free(line2);
+}
+
+bool update_lines_step_1(window_t *window, line_t *line1, line_t *line2)
+{
+    static bool step_1_finished = false;
+
+    if (step_1_finished)
+        return (false);
+    if (line2->point_b.y <= 0 && line2->color.g == 150) {
+        line1->point_a = (sfVector2f) {window->width / 4, window->height / 2};
+        line1->point_b = (sfVector2f) {window->width / 2, 0};
+        line1->color   = sfYellow;
+        line2->point_a = (sfVector2f) {window->width * 3/4, window->height / 2};
+        line2->point_b = (sfVector2f) {window->width / 2, window->height};
+        line2->color   = sfWhite;
+        step_1_finished = true;
+        return (false);
+    }
+    if (line1->point_b.y < window->height) {
+        line1->point_b.y++;
+    } else {
+        line1->point_b.x = 0;
+        line1->point_b.y = 0;
+        line1->color = (sfColor) {DARK_ORANGE, LINES_OPACITY};
+    }
+    if (line2->point_b.y > 0) {
+        line2->point_b.y--;
+    } else {
+        line2->point_b.x = window->width;
+        line2->point_b.y = window->height;
+        line2->color = (sfColor) {DARK_GREEN, LINES_OPACITY};
+    }
+    return (true);
 }
