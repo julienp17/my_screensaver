05780217aff05e5d9e4d35b57598a9c3ab750035
Julien Pause <julien.pause@epitech.eu>
2019-11-26 14:06:30 +0400

    Add include/random.h

---
+++ include/random.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2019
+** My Screensaver
+** File description:
+** Header file for random
+*/
+
+#ifndef RANDOM_H_
+    #define RANDOM_H_
+
+    float get_random_float(float const min, float const max);
+    sfColor get_random_color(unsigned int opacity);
+    sfColor get_black_or_white(void);
+#endif
