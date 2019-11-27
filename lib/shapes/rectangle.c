b862939aca072f26be9a901a060def65e8f25e0b
Julien Pause <julien.pause@epitech.eu>
2019-11-27 23:05:45 +0400

    Add rectangle shape in shapes library with draw_rectangle function in it

---
+++ lib/shapes/rectangle.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2019
+** My Screensaver
+** File description:
+** All functions needed to draw a rectangle
+*/
+
+#include <stdlib.h>
+#include "framebuffer.h"
+#include "shapes.h"
+
+void draw_rectangle(framebuffer_t *framebuffer, sfVector2i pos,
+                unsigned int width, unsigned int height, sfColor color)
+{
+    for (unsigned int row = pos.y ; row < (pos.y + height) ; row++)
+        for (unsigned int col = pos.x ; col < (pos.x + width) ; col++)
+            my_put_pixel(framebuffer, col, row, color);
+}
