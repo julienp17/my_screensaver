a1454aa82f656ceda3a0cce9660660111584daec
Julien Pause <julien.pause@epitech.eu>
2019-11-27 13:56:24 +0400

    Move animations.h from include/animations to just include/

--- include/animations.h
+++ include/animations.h
@@ -10,7 +10,7 @@
 
     #include "window.h"
 
-    #define MAX_ID          7
+    #define MAX_ID          8
 
     void (*get_animation_from_id(unsigned int nb))(window_t*);
     void rainbow_rain(window_t *window);
@@ -21,4 +21,5 @@
     void radar(window_t *window);
     void building_windows(window_t *window);
     void rgb_tan(window_t *window);
+    void photon_cannon(window_t *window);
 #endif
