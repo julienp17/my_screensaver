e0f52394a922526e35126ddefb5ce93a98298f04
Julien Pause <julien.pause@epitech.eu>
2019-11-27 13:55:59 +0400

    Refactor rainbow_rain to work with width structure elements rather than macros

--- src/animations/rainbow_rain.c
+++ src/animations/rainbow_rain.c
@@ -16,10 +16,10 @@ void rainbow_rain(window_t *window)
     droplet_t **droplets = malloc(sizeof(*droplets) * NB_DROPLETS);
 
     for (unsigned int i = 0 ; i < NB_DROPLETS ; i++)
-        droplets[i] = droplet_create();
+        droplets[i] = droplet_create(window);
     while (sfRenderWindow_isOpen(window->window)) {
         poll_events(window->window);
-        display_droplets(window->framebuffer, droplets);
+        display_droplets(window, droplets);
         window_refresh(window, &sfBlack);
     }
     for (unsigned int i = 0 ; i < NB_DROPLETS ; i++)
@@ -27,41 +27,41 @@ void rainbow_rain(window_t *window)
     free(droplets);
 }
 
-void display_droplets(framebuffer_t *framebuffer, droplet_t **droplets)
+void display_droplets(window_t *window, droplet_t **droplets)
 {
     for (unsigned int i = 0 ; i < NB_DROPLETS ; i++) {
         if (droplets[i]->beg.y > 0)
-            draw_line(framebuffer, droplets[i]->beg, droplets[i]->end,
+            draw_line(window->framebuffer, droplets[i]->beg, droplets[i]->end,
                         droplets[i]->color);
-        droplet_fall(droplets[i]);
+        droplet_fall(window, droplets[i]);
     }
 }
 
-droplet_t *droplet_create(void)
+droplet_t *droplet_create(window_t *window)
 {
     droplet_t *droplet = malloc(sizeof(*droplet));
 
-    droplet_get_random_state(droplet);
+    droplet_get_random_state(window, droplet);
     return (droplet);
 }
 
-void droplet_fall(droplet_t *droplet)
+void droplet_fall(window_t *window, droplet_t *droplet)
 {
     droplet->speed += ACCELERATION;
     droplet->beg.y += droplet->speed;
     droplet->end.y += droplet->speed;
-    if (droplet->beg.y >= W_HEIGHT)
-        droplet_get_random_state(droplet);
+    if (droplet->beg.y >= window->height)
+        droplet_get_random_state(window, droplet);
 }
-
-void droplet_get_random_state(droplet_t *droplet)
+    #include <stdio.h>
+void droplet_get_random_state(window_t *window, droplet_t *droplet)
 {
-    droplet->beg.x = get_random_float(0.0, (float)W_WIDTH);
-    droplet->beg.y = get_random_float(0.0, (float)(-W_HEIGHT));
+    droplet->beg.x = get_random_float(0.0, (float)window->width);
+    droplet->beg.y = get_random_float(0.0, (float)(-(int)(window->height)));
     droplet->len = get_random_float(5.0, 20.0);
     droplet->speed = get_random_float(0.0, 15.0);
-    droplet->color = (sfColor) {rand() % 255, rand() % 255, rand() % 255,
-                                rand() % (255 - 20) + 20};
+    droplet->color = get_random_color(rand() % 255);
     droplet->end.x = droplet->beg.x;
     droplet->end.y = droplet->beg.y + droplet->len;
+    //printf("droplet beg x = %f\tdroplet beg y = %f\n", droplet->beg.x, droplet->beg.y);
 }
